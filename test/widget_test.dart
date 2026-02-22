16961396060b11a7cdc4156f5639320d89fc1c3f
Ho Minh Quyen <minhquyenho09@gmail.com>
2026-02-22 09:58:23 -0500

    fix: register dependencies in widget test to fix CI

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'package:mihp/injection_container.dart';
 import 'package:mihp/main.dart';
 
 void main() {
+  setUpAll(() async {
+    // Đăng ký tất cả dependencies trước khi chạy test
+    await initDependencies();
+  });
+
   testWidgets('App should build without errors', (WidgetTester tester) async {
     await tester.pumpWidget(const MihpApp());
     expect(find.byType(MaterialApp), findsOneWidget);
